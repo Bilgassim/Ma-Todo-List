c0adaa239cd9526445e2126a12044b6f1ba83d78
Mohamed Boulkassoum <boulkassoum2002@gmail.com>
2025-05-27 21:35:37 +0100

    To do list

--- src/app/page.js
+++ src/app/page.js
@@ -1,103 +1,94 @@
-import Image from "next/image";
+"use client";
+import React, { useEffect, useState } from 'react'
 
-export default function Home() {
+export default function index() {
+  const [task, setTask] = useState([]);
+  const [newtask, setNewtask] = useState('');
+  // Pour les modifications
+  const [editingId, setEditingId] = useState(null);
+  const [editingText, setEditingText] = useState('');
+
+
+
+  useEffect(() => {
+    localStorage.setItem('tasks', JSON.stringify(task));
+  }, [task]);
+
+  useEffect(() => {
+    const tachesSauvegardees = JSON.parse(localStorage.getItem('tasks'));
+    if (tachesSauvegardees) {
+      setTask(tachesSauvegardees);
+    }
+  }, []);
+
+
+  const taches = task.map(tache => <li key={tache.id} style={{ textDecoration: tache.terminé ? 'line-through' : 'none' }}>
+    {editingId === tache.id ? (
+      <input
+        type="text"
+        value={editingText}
+        onChange={(e) => setEditingText(e.target.value)}
+        onBlur={() => saveEdit(tache.id)} // Sauvegarder en sortant du focus
+        autoFocus
+      />
+    ) : (
+      <span className='mx-2'>{tache.texte} </span>
+    )} 
+    <span className='mx-2'><button type='button' className=' text-sm hover:text-red-600 max-w-md space-y-1 text-red-400 list-disc list-inside' onClick={() => delText(tache.id)}> Supprimer la tâche</button></span>
+    <span><button type='button' className='mx-2 text-sm max-w-md space-y-1 text-green-400 hover:text-green-600 list-disc list-inside' onClick={() => toggleTermine(tache.id)}>Terminer</button></span>
+    <span><button className='mx-2 text-sm max-w-md space-y-1 text-yellow-400 hover:text-yellow-600 list-disc list-inside' onClick={() => startEdit(tache.id, tache.texte)}>Modifier</button></span>
+  </li>)
+
+  function addText(e) {
+    e.preventDefault();
+    if (newtask.trim() !== '') {
+      const newtache = {
+        id: Date.now(),
+        texte: newtask,
+        terminé: false
+      };
+      setTask([...task, newtache]);
+      setNewtask('')
+    }
+  }
+  function delText(id) {
+    const gardetache = task.filter(tache => tache.id != id)
+    setTask(gardetache)
+  }
+  function toggleTermine(id) {
+    const termine = task.map(
+      (fini) => {
+        if (fini.id === id) {
+          return { ...fini, terminé: !fini.terminé };
+        }
+        return fini
+      }
+    );
+    setTask(termine);
+  }
+  function startEdit(id, texte) {
+    setEditingId(id);
+    setEditingText(texte);
+  }
+  function saveEdit(id) {
+    const updatedTasks = task.map(t =>
+      t.id === id ? { ...t, texte: editingText } : t
+    );
+    setTask(updatedTasks);
+    setEditingId(null);
+    setEditingText('');
+  }
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <>
+      <form className='max-w-sm mx-auto' onSubmit={addText}>
+        <div className='mb-5'>
+          <label htmlFor='tache' className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Entrez votre tâche : </label>
+          <input id='tache' className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500' type="text" value={newtask} onChange={(e) => setNewtask(e.target.value)} placeholder='Ajouter une tâche...' />
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+        <button className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800' type='submit'>Ajouter</button>
+      </form>
+      <ul className='max-w-md space-y-1 text-gray-500 list-disc list-inside dark:text-gray-400'>{taches}</ul>
+    </>
+  )
 }
